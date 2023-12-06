65f9e0b4e1f30a6473327d4958643d531b369a69
Sunmarine <Sunmarine777@yandex.ru>
2023-12-07 01:16:22 +0300

    Completed for 50 percent

---
+++ Task2/Program.cs
@@ -0,0 +1,81 @@
+﻿//Задача 2: Задайте двумерный массив. Напишите программу, 
+//которая поменяет местами первую и последнюю строку массива.
+
+
+int rows = 3;
+int cols = 4;
+int[,] matrix = new int[rows,cols];
+
+Random rand = new Random();
+
+int[,] First = new int[rows,cols];
+int[,] Last = new int[rows,cols];
+int FirstStr = 0;
+int LastStr = 2;
+
+int[,] tempmatrix = new int[rows,cols];
+
+for (int i = 0; i < rows; i++)
+{
+      for (int j = 0; j < cols; j++)
+      {
+            matrix[i,j] = rand.Next(1,50);
+            Console.Write(matrix[i,j] + "\t");
+      }
+      Console.WriteLine();
+}
+
+for (int i = 0; i < rows; i++)
+{
+     for (int j = 0; j < cols; j++)
+      {
+            if (i == FirstStr)
+            {
+                  First[i,j] = matrix[i,j];
+                  FirstStr = i;
+            }
+      }
+}
+
+for (int i = 0; i < rows; i++)
+{
+      for (int j = 0; j < cols; j++)
+      {
+            if (i == LastStr)
+            {
+                  Last[i,j] = matrix[i,j];
+                  LastStr = i;
+            }
+      }
+}
+
+Console.WriteLine();
+
+for (int i = FirstStr, j = 0; j < cols; j++)
+{
+      
+      tempmatrix[i,j] = matrix[i,j]; // забрал первую
+}
+
+for (int i = LastStr, j = 0; j < cols; j++)
+{
+      matrix[FirstStr,j] = matrix[LastStr, j]; //первая строка получает из последней
+}
+
+for (int i = 0; i < rows; i++)
+{
+     for (int j = 0; j < cols; j++)
+      {
+            matrix[LastStr, j] = tempmatrix[i,j]; // последняя получает из первой, но нет
+      }
+}
+
+for (int i = 0; i < rows; i++)
+{
+     for (int j = 0; j < cols; j++)
+      {
+            Console.Write(matrix[i,j] + "\t");
+      }
+      Console.WriteLine();
+}
+Console.ReadKey();
